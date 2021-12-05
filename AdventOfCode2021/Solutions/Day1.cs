3a1d604bac7e35ada43ec249b1d06b317f3b8620
Matt DelBrocco <mvd@case.edu>
2021-12-04 22:31:39 -0500

    Day 2, Part 1 solved

---
+++ AdventOfCode2021/Solutions/Day1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2021.Solutions
+{
+  public class Day1
+  {
+    public static void Solve()
+    {
+      var depthReadings = File.ReadAllLines("../../../Inputs/Day1.txt");
+
+      Part1(depthReadings);
+      Part2(depthReadings);
+    }
+
+    // Count how many times the number increases from one to another
+    // https://adventofcode.com/2021/day/1
+    private static void Part1(string[] depthReadings)
+    {
+      var numberOfIncreases = 0;
+
+      for (int i = 0; i < depthReadings.Length - 1; i++)
+      {
+        var firstReading = int.Parse(depthReadings[i]);
+        var secondReading = int.Parse(depthReadings[i + 1]);
+
+        if (secondReading > firstReading)
+          numberOfIncreases++;
+      }
+
+      Console.WriteLine($"Part 1 Solution: {numberOfIncreases}");
+    }
+
+    // Count how many times a window of 3 numbers increases moving forward
+    // https://adventofcode.com/2021/day/1#part2
+    private static void Part2(string[] depthReadings)
+    {
+      var numberOfIncreases = 0;
+
+      for (int i = 0; i < depthReadings.Length - 3; i++)
+      {
+        var firstWindowSum = int.Parse(depthReadings[i]) + int.Parse(depthReadings[i + 1]) + int.Parse(depthReadings[i + 2]);
+        var secondWindowSum = int.Parse(depthReadings[i + 1]) + int.Parse(depthReadings[i + 2]) + int.Parse(depthReadings[i + 3]);
+
+        if (secondWindowSum > firstWindowSum)
+          numberOfIncreases++;
+      }
+
+      Console.WriteLine($"Part 2 Solution: {numberOfIncreases}");
+    }
+  }
+}
---
+++ AdventOfCode2021/Solutions/Day2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Solutions
+{
+  public class Day2
+  {
+    public static void Solve()
+    {
+      var lines = File.ReadAllLines("../../../Inputs/Day2.txt");
+
+      var movements = lines.Select(line =>
+      {
+        var tokens = line.Split(" ");
+        var dir = tokens.First();
+        var mag = int.Parse(tokens.Last());
+        return new Vector(dir, mag);
+      }).ToList();
+      Part1(movements);
+    }
+
+    // Figure out the submarine's ending position.
+    // https://adventofcode.com/2021/day/2
+    private static void Part1(List<Vector> movements)
+    {
+      var horizontalPosition = 0;
+      var depth = 0;
+
+      foreach (var movement in movements)
+      {
+        if (movement.Direction == "forward")
+        {
+          horizontalPosition += movement.Magnitude;
+        }
+        else if (movement.Direction == "down")
+        {
+          depth += movement.Magnitude;
+        }
+        else if (movement.Direction == "up")
+        {
+          depth -= movement.Magnitude;
+        }
+        else
+        {
+          Console.WriteLine("UNEXPECTED INPUT!");
+        }
+      }
+
+      Console.WriteLine($"Horizontal Position: {horizontalPosition}");
+      Console.WriteLine($"Depth: {depth}");
+      Console.WriteLine($"Multiplied Together: {horizontalPosition * depth}");
+    }
+
+    private static void Part2()
+    {
+      // TODO: after solving part 1
+    }
+
+    private class Vector
+    {
+      public string Direction { get; set; }
+      public int Magnitude { get; set; }
+
+      public Vector(string direction, int magnitude)
+      {
+        Direction = direction;
+        Magnitude = magnitude;
+      }
+    }
+  }
+}
